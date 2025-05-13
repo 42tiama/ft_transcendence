4f3c532871ab2c621b58536ebd835e7e690fd52a
Andrefcampos <andrecampos.wnr@gmail.com>
2025-05-13 16:03:05 -0300

    Update -> testes com google sigin-in

--- src/build-frontend.sh
+++ src/build-frontend.sh
@@ -6,7 +6,7 @@ fi
 
 echo "Building frontend..."
 
-tsc client/main.ts --outFile build/frontend/main.js
-cp client/index.html build/frontend/index.html
+# tsc frontend/main.ts --outFile build/frontend/main.js
+cp frontend/index.html build/frontend/index.html
 
 exit 0
