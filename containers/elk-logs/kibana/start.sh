95763731fec4028b0fbed7ba00eba0ed883f73fc
thaisnishimoto <thaismnishimoto@gmail.com>
2025-06-13 16:05:02 -0300

    WIP #12 - Fixed ssl configurations

--- containers/elk-logs/kibana/start.sh
+++ containers/elk-logs/kibana/start.sh
@@ -4,13 +4,15 @@
 /usr/share/kibana/bin/kibana &
 
 # Wait until Kibana is up
-until curl -s https://localhost:5601/api/status | grep -q '"level":"available"'; do
+until curl -s --cacert /usr/share/kibana/config/certs/ca/ca.crt \
+  -u elastic:$ELASTIC_PASSWORD https://localhost:5601/api/status | grep -q '"level":"available"'; do
   echo "Waiting for Kibana..."
   sleep 5
 done
 
 # Import the dashboards
-curl -X POST "https://localhost:5601/api/saved_objects/_import?overwrite=true" \
+curl -u elastic:$ELASTIC_PASSWORD \
+  -X POST --cacert /usr/share/kibana/config/certs/ca/ca.crt "https://localhost:5601/api/saved_objects/_import?overwrite=true" \
   -H "kbn-xsrf: true" \
   --form file=@/usr/share/kibana/dashboard.ndjson
 
