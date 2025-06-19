b8864bbd6bd46a62723394ce0d83ba2e72647ab7
iuryr <iuryroberto@gmail.com>
2025-06-18 22:20:42 -0300

    change src path of static files so that it works on dev container

--- src/frontend/client/client.ts
+++ src/frontend/client/client.ts
@@ -14,11 +14,13 @@ const server = fastify({
 
 server.register(fastifyStatic,
 {
-	root: "/" //root of static files webserver will be the root dir of the production container
+	//root: "/" CHANGE FOR PRODUCTION
+    root: "/fullbase/src" //root of static files webserver will be the root dir of the production container
 });
 
 server.get("/:path", (request, reply) => {
-    reply.sendFile("/build/static/html/index.html");
+    //reply.sendFile("/build/static/html/index.html");
+     reply.sendFile("./build/static/html/index.html");
 });
 
 server.listen({ host: "0.0.0.0", port: 8042 }, (err, address) => {
